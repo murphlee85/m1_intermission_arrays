de1594d3c2fccab94d7d57d311398e391299a865
Charles Lee <pcrlee85@gmail.com>
2018-04-29 22:18:14 -0600

    exit_ticket_1_2 finished

--- exit_ticket_1.rb
+++ exit_ticket_1.rb
@@ -1,12 +1,21 @@
 # TO DO:
 # Create an array of six elements - content of your choosing (either integers or strings!)
+array = ["basketball", "football", "soccer", "tennis", "hockey", "baseball"]
 # Access the third element
+array[2]
 # Add an element to the end of your array
+array.push("cricket")
 # Add an element to index 2
+array.insert(2,"badminton")
 # Remove the first element from your array
+array.shift
 # Remove any duplicates that may exist in your array
+array.uniq
 # Create a second array with 3 elements
+cars = ["Honda", "Acura", "Mercedes"]
 # "Sqash" the two arrays together
-
+array + cars
 # BONUS since we didn't cover today 🦄
 # If your array contains strings, write a sentence using each string. If it contains integers, create a multiplication table for your array.
+sports = array.join(', ')
+"I like to play all kinds of #{sports}"
--- exit_ticket_2.rb
+++ exit_ticket_2.rb
@@ -14,27 +14,43 @@ pet.length
 # YOUR TURN:
 
 hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix"]
+# hedgies is the variable name that has been assigned to the array of strings
 new_hedgies = ["Mooshi", "Monkey"]
-
+# new_hedgies is a different variable name assigned to the array of strings of "Mooshi" and "Monkey"
 hedgies[3]
+# hedgies[3] will access the 3rd element in the array which should be "Ernie"
 hedgies.empty?
+# this is checking to see if the hedgies array is empty and since it isn't, it will return false
 hedgies.at[5]
+# this will access the 5th element in the array, which should be "Margo"
 hedgies.unshift("Bella")
+# this will add the string "Bella" to the beginning of the array. It will occupy the first position in the array
 hedgies.insert(6, "Onyx")
+# this will add the string "Onyx" to the 6th position in the array
 hedgies.compact
+# this method will remove all instances of nil in the array. In this case, it will return the same array because there are no nil values
 hedgies.pop
+# this method will remove the last element in the array, which should be the string "Trix"
 hedgies.fetch(0)
+# this method will return the 1st element in the array, which should be the string "Alvin"
 hedgies.include?("Bagel")
+# this method is looking to see if the string "Bagel" is in the array and since it isn't, it will return false
 hedgies.shift
+# this method will remove the 1st element in the array, which should be "Alvin"
 hedgies.push("Toast")
+# this method will add the string "Toast" to the end of the array
 hedgies << "Rocky"
+# the << symbols indicates that it will shovel in the string "Rocky" to the end of the array
 hedgies.count
-# also, what is at least one other way to accomplish this?
+# hedgies.length
 hedgies.uniq!
-# also, how would this be different if we omitted the "!"?
+# the uniq method removes all instances of nil in the array
+# using the method with the exclamation point will return nil, the method without the exclamation point will return the original array
 hedgies.concat(new_hedgies)
+# this method will add the elements from new_hedgies to hedgies
 
 # BONUS since we didn't cover today 🦄
 hedgies.each do |hedgie|
   puts hedgie
 end
+# this enumerable will iterate through each element in the array and will return each element in the array
